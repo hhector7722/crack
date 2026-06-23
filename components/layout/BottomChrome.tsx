4d9bd8f547c6647b2318fed538bd096b4bed5e29
Hector <hhector7722@gmail.com>
2026-06-23 04:46:54 +0200

    +

--- components/layout/BottomChrome.tsx
+++ components/layout/BottomChrome.tsx
@@ -17,18 +17,16 @@ export function BottomChrome() {
   return (
     <div
       data-tm-bottom-chrome
-      className="tm-bottom-chrome fixed bottom-0 left-0 right-0 z-[95] flex flex-col items-center bg-[var(--tm-tabbar-bg-hex)] pb-safe"
+      className="tm-bottom-chrome fixed bottom-0 left-0 right-0 z-[95] flex flex-col items-center bg-[var(--tm-tabbar-bg-hex)]"
     >
-      <div className="flex h-[var(--tm-chrome-fab)] w-full max-w-[430px] shrink-0 items-center justify-center">
-        <button
-          type="button"
-          onClick={openCaptureMenu}
-          aria-label="Crear"
-          className="flex h-12 min-h-12 w-12 min-w-12 shrink-0 items-center justify-center rounded-full bg-zinc-100 text-zinc-950 shadow-lg shadow-black/40 transition-transform active:scale-95"
-        >
-          <Plus className="h-6 w-6" strokeWidth={2} />
-        </button>
-      </div>
+      <button
+        type="button"
+        onClick={openCaptureMenu}
+        aria-label="Crear"
+        className="tm-fab absolute left-1/2 top-0 z-[96] flex h-12 w-12 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-zinc-100 text-zinc-950 shadow-xl shadow-black/50 transition-transform active:scale-90"
+      >
+        <Plus className="h-6 w-6" strokeWidth={2} />
+      </button>
 
       <PagerDots />
       <TabBar />
