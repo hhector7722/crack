76af989d18427c4eda000b949031fb624e667c2a
Hector <hhector7722@gmail.com>
2026-07-11 18:35:41 +0200

    sd

---
+++ supabase/migrations/005_add_file_type.sql
@@ -0,0 +1,6 @@
+alter table public.items
+  drop constraint if exists items_type_check;
+
+alter table public.items
+  add constraint items_type_check
+    check (type in ('note', 'image', 'audio', 'file'));
